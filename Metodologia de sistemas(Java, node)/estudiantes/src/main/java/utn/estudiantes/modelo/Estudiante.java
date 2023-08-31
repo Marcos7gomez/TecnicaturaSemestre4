e1e9675574be8f4d20f4a209505e9eee49fa48f8
Marcos Gomez <marcoslg.gomez@gmail.com>
2023-08-30 21:16:35 -0300

    Cambios en Java
    Arreglos en el repo y actualizacion de clase 3 de java

---
+++ Metodologia de sistemas(Java, node)/estudiantes/src/main/java/utn/estudiantes/modelo/Estudiante.java
@@ -0,0 +1,26 @@
+package utn.estudiantes.modelo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+//boilerplate - es código repetitivo
+@Data // Crea los métodos get y set
+@NoArgsConstructor // Agrega el constructor vacío(Sin argumentos)
+@AllArgsConstructor //Agrega constructor con todos los argumentos
+@ToString // Agrega método to string
+public class Estudiante {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idEstudiante;
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private String email;
+}
