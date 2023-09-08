4bc54e39d54aa8b8b3ccd6e8568e529902673d04
Marcos Gomez <marcoslg.gomez@gmail.com>
2023-09-08 12:11:54 -0300

    Clase 4 de java completa

--- Metodologia-de-sistemas/Java/estudiantes/src/main/java/utn/estudiantes/servicio/EstudianteServicio.java
+++ Metodologia-de-sistemas/Java/estudiantes/src/main/java/utn/estudiantes/servicio/EstudianteServicio.java
@@ -1,5 +1,37 @@
 package utn.estudiantes.servicio;
 
-public class EstudianteServicio implements IEstudianteServicio{
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import utn.estudiantes.modelo.Estudiante;
+import utn.estudiantes.repositorio.EstudianteRepositorio;
 
+import java.util.List;
+
+@Service
+public class EstudianteServicio implements IEstudianteServicio{
+    @Autowired
+    private EstudianteRepositorio estudianteRepositorio;
+
+    @Override
+    public List<Estudiante> listarEstudiantes() {
+        List<Estudiante> estudiantes = estudianteRepositorio.findAll(); //Con esto nos regresa todos los elementos de la lista
+        return estudiantes;
+    }
+
+    @Override
+    public Estudiante buscarEstudiantePorId(Integer idEstudiante) {
+        Estudiante estudiante = estudianteRepositorio.findById(idEstudiante).orElse(null); // orElseThrow para atrapar cualquier tipo de excepción
+        return estudiante;
+    }
+
+    @Override
+    public void guardarEstudiante(Estudiante estudiante) {
+        estudianteRepositorio.save(estudiante);
+    }
+
+    @Override
+    public void eliminarEstudiante(Estudiante estudiante) {
+        estudianteRepositorio.delete(estudiante);
+
+    }
 }
