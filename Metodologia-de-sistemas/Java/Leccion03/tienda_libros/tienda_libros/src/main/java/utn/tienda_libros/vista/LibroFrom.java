874cd2ab232045ecdced4662a291089ce5e0fad1
Marcos Gomez <marcoslg.gomez@gmail.com>
2023-10-19 19:47:23 -0300

    Clase 9 de java completa
    Metodo agregar funcionando correctamente

--- Metodologia-de-sistemas/Java/Leccion03/tienda_libros/tienda_libros/src/main/java/utn/tienda_libros/vista/LibroFrom.java
+++ Metodologia-de-sistemas/Java/Leccion03/tienda_libros/tienda_libros/src/main/java/utn/tienda_libros/vista/LibroFrom.java
@@ -3,7 +3,7 @@ package utn.tienda_libros.vista;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import utn.tienda_libros.servicio.LibroServicio;
-
+import utn.tienda_libros.modelo.Libro;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
@@ -13,13 +13,24 @@ public class LibroFrom extends JFrame {
     LibroServicio libroServicio;
     private JPanel panel;
     private JTable tablaLibros;
+    private JLabel Libro;
+    private JTextField libroTexto;
+    private JLabel Autor;
+    private JTextField autorTexto;
+    private JLabel Precio;
+    private JTextField precioTexto;
+    private JTextField existenciasTexto;
+    private JLabel Existencias;
+    private JButton agregarButton;
+    private JButton modificarButton;
+    private JButton eliminarButton;
     private DefaultTableModel tablaModeloLibros;
 
     @Autowired
     public LibroFrom(LibroServicio libroServicio){
         this.libroServicio = libroServicio;
         iniciarForma();
-
+        agregarButton.addActionListener(e -> agregarLibro());
     }
 
     private void iniciarForma(){
@@ -35,6 +46,40 @@ public class LibroFrom extends JFrame {
         int y = (tamanioPantalla.height - getHeight()/2);
         setLocation(x, y);
 
+    }
+
+    private void agregarLibro(){
+        // Leer los valores del formulario
+        if(libroTexto.getText().equals("")){
+            mostrarMensaje("Ingresa el nombre del libro");
+            libroTexto.requestFocusInWindow();
+            return;
+        }
+        var nombreLibro = libroTexto.getText();
+        var autor = autorTexto.getText();
+        var precio = Double.parseDouble(precioTexto.getText());
+        var existencias = Integer.parseInt(existenciasTexto.getText());
+        // Creamos el objeto libro
+        var libro = new Libro(null, nombreLibro, autor, precio, existencias);
+//        libro.setNombreLibro(nombreLibro);
+//        libro.setAutor(autor);
+//        libro.setPrecio(precio);
+//        libro.setExistencias(existencias);
+        this.libroServicio.guardarLibro(libro);
+        mostrarMensaje("Se agregó el libro...");
+        limpiarFormulario();
+        listarLibros();
+    }
+
+    private void limpiarFormulario(){
+        libroTexto.setText("");
+        autorTexto.setText("");
+        precioTexto.setText("");
+        existenciasTexto.setText("");
+    }
+
+    private void mostrarMensaje(String mensaje){
+        JOptionPane.showMessageDialog(this, mensaje);
     }
 
     private void createUIComponents() {
@@ -53,7 +98,7 @@ public class LibroFrom extends JFrame {
         // Obtener los libros de la BD
         var libros = libroServicio.listarLibros();
         // Iteramos cada libro
-        libros.forEach((libro)->{// Funcion lambda
+        libros.forEach((libro)->{// Función lambda
             //Creamos cada registro para agregarlos a la tabla
             Object [] renglonLibro = {
                     libro.getIdLibro(),
