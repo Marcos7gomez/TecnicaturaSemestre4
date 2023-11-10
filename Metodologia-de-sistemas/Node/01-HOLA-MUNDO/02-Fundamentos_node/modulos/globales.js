0f6f4bd763c7e5eff80aa2425c6f50069e1804f3
Marcos Gomez <marcoslg.gomez@gmail.com>
2023-11-10 19:26:41 -0300

    Clase 13 de node agregada

---
+++ Metodologia-de-sistemas/Node/01-HOLA-MUNDO/02-Fundamentos_node/modulos/globales.js
@@ -0,0 +1,16 @@
+// this === global = true
+// Mostrar algo en consola
+console.log();
+
+//Mensaje en forma de error
+console.error();
+
+//Ejecutar un codigo luego de un intervalo de tiempo
+setTimeout(()=>{});
+
+//Ejecutar un codigo cada intervalo de tiempo
+setInterval(()=>{});
+
+SetImmediate(()=>{});
+
+console.log(setInterval);
